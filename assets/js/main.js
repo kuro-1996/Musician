cb785d5654c417779721627b1fd6fa6f1ec3d21d
kuro96 <ng.ngocanhtuan96@gmail.com>
2020-01-16 11:06:34 +0700

    update code (add header + banner + discography + concert section )

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,23 @@
-$('.dropdown').on('click', function(e) {
-    var target = $(e.target);
-    var dropdown = target.closest('.dropdown');
-    return !dropdown.hasClass('open') || !target.hasClass('dropdown-menu__search__input');
+$(document).ready(function () {  
+    //show menu and change icon after click btn
+    $('.dropdown-btn').click(function () { 
+        $('.dropdown-btn__icon , .menu').toggleClass('active');
+        $('header').toggleClass('change');
+    })
+    //hide menu and scroll to section after click menu item
+    $('.menu a').click(function (e) {   
+        e.preventDefault();
+        $(".menu , .menu a , .dropdown-btn__icon").removeClass("active");
+        $("header").removeClass("change");
+        $(this).addClass('active');
+        $("html,body").animate({
+            scrollTop: $($(this).prop("hash")).offset().top
+        }, 'slow');
+    })
+    //change header background if scroll over disco section
+    $(window).scroll(function () {  
+        if ($(this).scrollTop() > $('.disco').offset().top)
+            $("header").addClass('active');
+        else $("header").removeClass("active"); 
+    })
 });
